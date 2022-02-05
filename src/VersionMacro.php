3d9309dbc33784170f91ad87dd6a741d82b48517
Tomáš Kudělka <thorewi@gmail.com>
2022-02-05 11:17:21 +0100

    Update VersionMacro.php

--- src/VersionMacro.php
+++ src/VersionMacro.php
@@ -8,68 +8,62 @@ use \Latte\Macros\MacroSet;
 use \Latte\PhpWriter;
 use \Nette\InvalidArgumentException;
 
-class VersionMacro {
+final class VersionMacro
+{
+	static private ?int $timestamp = null;
+	static private ?string $timestampFile = null;
 
-	use \Nette\SmartObject;
-	
-	/** @var array */
-	protected $parameters;
-
-	/** @var string */
-	protected $appDir;
-
-	public function __construct($parameters, $appDir) {
-		$this->parameters = $parameters;
-		$this->appDir = $appDir;
-	}
-
-	public function install(Compiler $compiler) {
-		$me = new MacroSet($compiler);
-		$me->addMacro('v', array($this, 'macroVersion'));
-		$me->addMacro('vn', array($this, 'macroVersionNumber'));
-		return $me;
-	}
-
-	/**
-	 * Usage:
-	 * <script type="text/javascript" src="/js/myJsFile{v}"></script>
-	 */
-	public function macroVersion(MacroNode $node, PhpWriter $writer)
+	public function setTimestampFile(string $timestampFile): self
 	{
-		return "echo '?v=". $this->getVersion() ."'";
-	}
-
-	/**
-	 * Usage:
-	 * <script type="text/javascript" src="/js/myJsFile?v={vn}"></script>
-	 */
-	public function macroVersionNumber(MacroNode $node, PhpWriter $writer)
-	{
-		return "echo '". $this->getVersion() ."'";
-	}
-
-	public function getVersion() {
-		static $out = NULL;
-
-		if ($out !== NULL) {
-			return $out;
-		}
-
-		$timestampFile = NULL;
-
-		// Necháváme kvůli zpětné kompatibilitě:
-		$timestampFile = isset($this->parameters['htdeployment'])
-			? $this->parameters['htdeployment']
-			: $this->appDir . '/../.htdeployment';
-
-		if (isset($this->parameters['timestampFile'])) {
-			$timestampFile = $this->parameters['timestampFile'];
-		}
-
 		if (! is_file($timestampFile)) {
 			throw new InvalidArgumentException("File '$timestampFile' not found or is not a file!");
 		}
 
-		return filemtime($timestampFile);
+		self::$timestampFile = $timestampFile;
+
+		return $this;
+	}
+
+	public static function install(Compiler $compiler): MacroSet
+	{
+		$me = new MacroSet($compiler);
+		$me->addMacro('v', array(__CLASS__, 'macroVersion'));
+		$me->addMacro('vn', array(__CLASS__, 'macroVersionNumber'));
+		return $me;
+	}
+
+	/**
+	 * @internal
+	 * Usage:
+	 * <script type="text/javascript" src="/js/myJsFile{v}"></script>
+	 */
+	public static function macroVersion(MacroNode $node, PhpWriter $writer): string
+	{
+		return "echo '?v=". self::getVersion() ."'";
+	}
+
+	/**
+	 * @internal
+	 * Usage:
+	 * <script type="text/javascript" src="/js/myJsFile?v={vn}"></script>
+	 */
+	public static function macroVersionNumber(MacroNode $node, PhpWriter $writer): string
+	{
+		return "echo '" . self::getVersion() . "'";
+	}
+
+	private static function getVersion(): int
+	{
+		if (self::$timestamp) {
+			return self::$timestamp;
+		}
+
+		self::$timestamp = time();
+
+		if (self::$timestampFile) {
+			self::$timestamp = filemtime(self::$timestampFile);
+		}
+
+		return self::$timestamp;
 	}
 }
